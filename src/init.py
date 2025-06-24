a3940177be57ed3415c2be32b002988e0623a3f5
Yami-no-karuro <carloborgna98@gmail.com>
2025-06-24 14:12:36 +0200

    Init command refactoring

--- src/init.py
+++ src/init.py
@@ -11,32 +11,33 @@ from sqlite3 import Connection
 import os
 import json
 
-def init(destination: str):
-    db_path: str = f"{destination}/.safesync/data/db/safesync-core.db"
+def init(dest_path: str):
+    db_path: str = os.path.join(dest_path, ".safesync", "data", "db", "safesync-core.db")
 
-    init_root(destination)
-    init_metadata(destination)
+    init_root(dest_path)
+    init_metadata(dest_path)
     init_db(db_path)
-    source_dir(destination, db_path)
+    source_dir(dest_path, db_path)
 
-def init_root(path: str):
-    root: str = f"{path}/.safesync"
+def init_root(dest_path: str):
+    root_path: str = os.path.join(dest_path, ".safesync")
 
-    db_dir: str = os.path.join(root, 'data', 'db')
-    objects_dir: str = os.path.join(root, 'data', 'objects')
-    os.makedirs(db_dir, exist_ok = True)
-    os.makedirs(objects_dir, exist_ok = True)
+    db_dir_path: str = os.path.join(root_path, 'data', 'db')
+    objects_dir_path: str = os.path.join(root_path, 'data', 'objects')
+    os.makedirs(db_dir_path, exist_ok = True)
+    os.makedirs(objects_dir_path, exist_ok = True)
 
-def init_metadata(path: str):
-    metadata_path: str = os.path.join(path, '.safesync', 'metadata.json')
-    metadata: dict = {
+def init_metadata(dest_path: str):
+    root_path : str = os.path.join(dest_path, ".safesync")
+    metadata_path: str = os.path.join(root_path, 'metadata.json')
+    data: dict = {
         "time": datetime.now().timestamp(),
-        "source": path,
-        "root": f"{path}/.safesync"
+        "source": dest_path,
+        "root": root_path
     }
 
     with open(metadata_path, 'w') as json_file:
-        json.dump(metadata, json_file, indent = 4)
+        json.dump(data, json_file, indent = 4)
 
     print(f"File \"{metadata_path}\" successfully created.")
 
