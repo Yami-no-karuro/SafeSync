9d8498c1e8fa186211be3f3c92aa84ac8f140b1e
Yami-no-karuro <carloborgna98@gmail.com>
2025-06-24 14:00:43 +0200

    Init command refactoring

--- src/init.py
+++ src/init.py
@@ -5,25 +5,40 @@ from lib.sqlite import sqlite_connect
 from lib.sqlite import sqlite_execute
 from lib.sqlite import sqlite_fetchone
 
+from datetime import datetime
 from sqlite3 import Connection
 
 import os
+import json
 
 def init(destination: str):
     db_path: str = f"{destination}/.safesync/data/db/safesync-core.db"
 
-    create_root(destination)
+    init_root(destination)
+    init_metadata(destination)
     init_db(db_path)
     source_dir(destination, db_path)
 
-def create_root(path: str):
+def init_root(path: str):
     root: str = f"{path}/.safesync"
-    data_dir: str = os.path.join(root, 'data')
-    db_dir: str = os.path.join(data_dir, 'db')
-    objects_dir: str = os.path.join(data_dir, 'objects')
 
+    db_dir: str = os.path.join(root, 'data', 'db')
+    objects_dir: str = os.path.join(root, 'data', 'objects')
     os.makedirs(db_dir, exist_ok = True)
     os.makedirs(objects_dir, exist_ok = True)
+
+def init_metadata(path: str):
+    metadata_path: str = os.path.join(path, '.safesync', 'metadata.json')
+    metadata: dict = {
+        "time": datetime.now().timestamp(),
+        "source": path,
+        "root": f"{path}/.safesync"
+    }
+
+    with open(metadata_path, 'w') as json_file:
+        json.dump(metadata, json_file, indent = 4)
+
+    print(f"File \"{metadata_path}\" successfully created.")
 
 def init_db(db_path: str):
     conn: Connection = sqlite_connect(db_path)
