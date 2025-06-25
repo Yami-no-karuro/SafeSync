1b76940cf9b9ff71a7fdeb0a9f00d6186ce24bcc
Yami-no-karuro <carloborgna98@gmail.com>
2025-06-25 09:38:36 +0200

    Status command updates

--- src/source.py
+++ src/source.py
@@ -2,6 +2,7 @@ from lib.libhash.bindings import fnv1a
 from lib.libhash.bindings import fnv1a_file
 
 from lib.sqlite import sqlite_fetchone
+from lib.sqlite import sqlite_fetchall
 
 from src.db import add_state
 from src.db import add_source
@@ -45,26 +46,44 @@ def status_dir(conn: Connection, dir_path: str):
         print("Exiting...")
         sys.exit(1)
 
+    sources: list[tuple] | None = sqlite_fetchall(conn, """
+        SELECT * FROM sources
+        WHERE state = ?
+    """, (state,))
+    if sources is None:
+        print("Unable to fetch the latest state.")
+        print("Exiting...")
+        sys.exit(1)
+
+    sources_dict: dict = {}
+    for source in sources:
+        sources_dict[source[3]] = {
+            "id": source[0],
+            "state": source[1],
+            "path": source[2],
+            "path_hash": source[3],
+            "content_hash": source[4]
+        }
+
     for root, _dirs, files in os.walk(dir_path):
         for file in files:
-            status_file(conn, state, os.path.join(root, file))
+            status_file(sources_dict, os.path.join(root, file))
+
+    for remaining in sources_dict:
+        print(f"MODIFIED: \"{remaining['path']}\" ({remaining['path_hash']})")
 
     print(f"Status for directory \"{dir_path}\" completed successfully.")
 
-def status_file(conn: Connection, state: int, file_path: str):
+def status_file(sources: dict, file_path: str):
     file_path_hash: str = hex(fnv1a(file_path.encode()))[2:]
     content_hash: str = hex(fnv1a_file(file_path))[2:]
 
-    entry: tuple | None = sqlite_fetchone(conn, """
-        SELECT * FROM sources
-        WHERE state = ? AND
-        path_hash = ?
-    """, (state, file_path_hash))
-
-    if entry is None:
+    if file_path_hash in sources:
+        entry: dict = sources[file_path_hash]
+        if entry["content_hash"] != content_hash:
+            print(f"MODIFIED: \"{file_path}\" ({file_path_hash})")
+    else:
         print(f"NEW: \"{file_path}\" ({file_path_hash})")
         return
 
-    e_id, e_state, e_path, e_path_hash, e_content_hash = entry
-    if content_hash != e_content_hash:
-        print(f"MODIFIED: \"{file_path}\" ({file_path_hash})")
+    del sources[file_path_hash]
