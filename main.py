48005ab08daa45f2ccfcc0c33a76c2a9df7beeea
Yami-no-karuro <carloborgna98@gmail.com>
2025-06-30 19:20:44 +0200

    Working in CWD

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.commands.snap import snap
 from src.commands.status import status
 
 import sys
+import os
 
 def print_help():
     print(f"Usage: safesync <command> [<args>]")
@@ -15,17 +16,16 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print_help()
         sys.exit(1)
-
+        
+    working_dir: str = os.getcwd()
     command: str = sys.argv[1]
-    if command == "init" and len(sys.argv) == 3:
-        destination: str = sys.argv[2]
-        init(destination)
-    elif command == "status" and len(sys.argv) == 3:
-        destination: str = sys.argv[2]
-        status(destination)
-    elif command == "snap" and len(sys.argv) == 3:
-        destination: str = sys.argv[2]
-        snap(destination)
+    
+    if command == "init":
+        init(working_dir)
+    elif command == "status":
+        status(working_dir)
+    elif command == "snap":
+        snap(working_dir)
     else:
         print_help()
         sys.exit(1)
