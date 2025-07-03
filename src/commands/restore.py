5328a672a866249a9729cab1b19f767034e25392
Yami-no-karuro <carloborgna98@gmail.com>
2025-07-03 09:26:21 +0200

    Restore command review

--- src/commands/restore.py
+++ src/commands/restore.py
@@ -1,8 +1,7 @@
-from lib.libcompress.bindings import huf_decompress
 from lib.sqlite import sqlite_connect
 from src.entities.state import get_state
 from src.entities.state import get_latest_state
-from src.entities.sources import get_sources
+from src.restorer import restore_directory
 
 from sqlite3 import Connection
 
@@ -15,6 +14,7 @@ def restore(dest_path: str, trg_state_id: int):
         print("Not a SafeSync repository.")
         sys.exit(1)
 
+    ignore_path: str = os.path.join(dest_path, ".syncignore")
     data_path: str = os.path.join(root_path, "data")
     db_path: str = os.path.join(data_path, "safesync-core.db")
     conn: Connection = sqlite_connect(db_path)
@@ -26,11 +26,7 @@ def restore(dest_path: str, trg_state_id: int):
         conn.close()
         sys.exit(1)
         
-    trg_sources: dict = get_sources(conn, trg_state["id"])
-    for key in trg_sources:
-        trg_source: dict = trg_sources[key]
-        huf_decompress(trg_source["obj_path"], trg_source["path"])
-        print(f"Object \"{trg_source['obj_path']}\" successfully restored.")
+    restore_directory(conn, dest_path, ignore_path, trg_state["id"])
   
     print(f"Previous State: {lts_state['id']} ({lts_state['time']}).")
     print(f"Roll-back state: {trg_state['id']} ({trg_state['time']}).")
--- src/restorer.py
+++ src/restorer.py
@@ -1,9 +1,18 @@
+from lib.libhash.bindings import fnv1a
+from lib.libcompress.bindings import huf_decompress
+from src.entities.sources import get_sources
+from src.utils.ignore import load_ignores
+
 from sqlite3 import Connection
 
 import os
 
-def restore_directory(conn: Connection, target_path: str, ignores: list):
+def restore_directory(conn: Connection, target_path: str, ignore_path: str, target_state_id: int):
+    trg_sources: dict = get_sources(conn, target_state_id)
+    
+    ignores: list = load_ignores(ignore_path)
     ignores.append(".safesync");
+    
     for root, _dirs, files in os.walk(target_path):
         ignored: bool = False
         crnt: list = root.split(os.sep)
@@ -15,4 +24,13 @@ def restore_directory(conn: Connection, target_path: str, ignores: list):
             continue
             
         for file in files:
-            print(file)
+            restore_file(file, trg_sources)
+
+def restore_file(file_path: str, sources: dict):
+    file_path_hash: str = hex(fnv1a(file_path.encode()))[2:]
+    if file_path_hash in sources:
+        source: dict = sources[file_path_hash]
+        huf_decompress(source["obj_path"], source["path"])
+        print(f"Object \"{source['obj_path']}\" successfully restored.")
+    else:
+        os.remove(file_path)
